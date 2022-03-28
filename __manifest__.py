39a5257e631e35bdbd439014f8bbede33a6ea897
usmanalvi007 <usmanawan1211@gmail.com>
2022-03-28 20:16:39 +0000

    adding fone zone report on a github

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Fone Zone Quotation Report',
+    'version': '1.0',
+    'summary': 'Sale Quotation Pdf report',
+    'sequence': 10,
+    'description': """FoneZone Sale Quotation Report""",
+    'category': 'sales',
+    'website': 'https://www.mediodconsulting.com',
+    'images': [],
+    'depends': ['sale'],
+    'data': [
+        'reports/sale_quotation_report.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
